3a22fdd1e0fc53b4d3939532a6f34195c6d9b285
Izam Mohammed <izamdeveloper1@gmail.com>
2024-01-05 00:27:10 +0530

    completed the price ingestion in notebook

---
+++ src/TradePilot/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+DATA_CONFIG_FILE_PATH = Path("config/data_config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
